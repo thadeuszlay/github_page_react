c25698f13f53bb31b433dcdd87e9394b7b783cb0
thanle <thanh.le@sinnerschrader.com>
2018-10-20 23:15:51 +0200

    Updates

---
+++ precache-manifest.cf3bf0b3bfb12f8d16b7e95f93ca53d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_page_react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "83ccc9543cdc3ff11392",
+    "url": "/github_page_react/static/js/runtime~main.83ccc954.js"
+  },
+  {
+    "revision": "522349c1e4d9cb19a9b2",
+    "url": "/github_page_react/static/js/main.522349c1.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/github_page_react/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "522349c1e4d9cb19a9b2",
+    "url": "/github_page_react/static/css/main.5114113a.chunk.css"
+  },
+  {
+    "revision": "b0abb8a39413b78f1b01576706f04fdb",
+    "url": "/github_page_react/index.html"
+  }
+];
